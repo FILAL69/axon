ff870b24a66edabecb56bfb4081d591191c4edc3
FILAL69 <filaliyanis4@gmail.com>
2025-08-01 02:19:35 +0100

    Create try.js

---
+++ pages/export default function TryAssistant() {   return (     <div style={{       height: '100vh',       display: 'flex',       justifyContent: 'center',       alignItems: 'center',       fontSize: '24px'     }}>       🚀 This is the assistant page!     </try.js
@@ -0,0 +1,13 @@
+export default function TryAssistant() {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '24px'
+    }}>
+      🚀 This is the assistant page!
+    </div>
+  );
+}
