beb8dc50d3adfcf5b3349197881e1cb626b2a3df
FILAL69 <filaliyanis4@gmail.com>
2025-08-07 12:42:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-<a 
-  href="/try" 
-  style={{
-    marginTop: '1.5rem',
-    padding: '1rem 2rem',
-    backgroundColor: '#000',
-    color: '#fff',
-    textDecoration: 'none',
-    borderRadius: '8px'
-  }}
->
-  Try VaultAI
-</a>
+export default function Home() {
+  return (
+    <a
+      href="/try"
+      style={{
+        marginTop: '1.5rem',
+        padding: '1rem 2rem',
+        backgroundColor: '#000',
+        color: '#fff',
+        textDecoration: 'none',
+        borderRadius: '8px'
+      }}
+    >
+      Try VaultAI
+    </a>
+  );
+}
+
